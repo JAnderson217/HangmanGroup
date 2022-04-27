2a14da3055f1394de5feca585d959e797d3ad7ec
JAnderson217 <24385176+JAnderson217@users.noreply.github.com>
2022-04-27 21:44:20 +0100

    Added getting word from txt file + validation

--- HangmanGroup/Program.cs
+++ HangmanGroup/Program.cs
@@ -2,27 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace HangmanGroup
 {
     internal class Program
     {
+        private static string[] listOfWords = { "apple", "cat", "banana", "elephant", "football"
+        , "tree", "dancer", "gymnast", "temperature", "wedding", "chimney", "zebra", "glass"};
+        private static string secretWord = "";
+        private static string currentWord;
+        private static string input = "";
+        private static int lives = 0;
+        private static List<string> guesses = new List<string>();
+
         static void Main(string[] args)
         {
-            string[] listOfWords = { "apple", "cat", "banana", "elephant"};
-            Random random = new Random();
-            string secretWord = listOfWords[random.Next(listOfWords.Length)];
-            //currentWord as secretWord but all _
-            string currentWord = new String('_', secretWord.Length);
-            string input = "";
-            int guesses = 0;
-            while (guesses < 9 && !secretWord.Equals(currentWord))
+            //gets the word to guess
+            getWord();
+            //main game loop - keeps running until word guessed or 9 lives used
+            while (lives < 9 && !secretWord.Equals(currentWord))
             {
-                //Console.WriteLine($"Word to guess is {secretWord}");
-                Console.WriteLine($"Word to guess is {currentWord}");
-                Console.WriteLine("Enter a letter to guess!");
-                input = Console.ReadLine();
+                //several methods that need to run in loop, getting users guess, checking guess
+                //printing status of game etc
+                Console.WriteLine();
+                Console.WriteLine($"Word to guess is {string.Join("", currentWord)}");
+                getGuess();
+                //print list of guesses
+                showGuesses();
+                Console.WriteLine();
+                //checks guess from user, if user guessed a letter correctly, replace _ with letter
                 if (checkGuess(secretWord, input))
                 {
                     string tempString = "";
@@ -42,9 +52,9 @@ namespace HangmanGroup
                 }
                 else
                 {
-                    guesses++;
+                    lives++;
                 }
-                drawHangman(guesses);
+                drawHangman(lives);
             }
             if (secretWord.Equals(currentWord))
             {
@@ -54,21 +64,27 @@ namespace HangmanGroup
             {
                 Console.WriteLine($"Unlucky, the word was {secretWord}.");
             }
-            Console.ReadLine();
+            restart();
         }
 
         static bool checkGuess(string a, string b)
         {
-            if (a.Contains(b))
+            //method to check if users guess is correct, returns bool
+            if (a.Equals(b))
             {
-                return true;
+                //ends game as word has been guessed
+                currentWord = secretWord;
             }
-            return false    ;
+            else if (a.Contains(b) && b.Length == 1) 
+            { 
+                return true; 
+            }
+            return false;
         }
 
         static void drawHangman(int guesses)
         {
-            //draw Hangman stick figure
+            //draws Hangman figure, has each stage stored in array corresponding to lives used
             Console.WriteLine($"Lives remaining: {9-guesses}");
             string[] Hangman = new string[] { "",
                 "\n" + "\n | " + "\n | " + "\n | " + "\n | " + "\n | " + "\n |_______________________\n",
@@ -80,7 +96,93 @@ namespace HangmanGroup
                 "\n___________________" + "\n|                  |" + "\n|                  O" + "\n|               ---|---" + "\n|" + "\n|" + "\n|_______________________\n",
                 "\n___________________" + "\n|                  |" + "\n|                  O" + "\n|               ---|---" + "\n|                  /" + "\n|                 /" + "\n|_______________________\n",
                 "\n___________________" + "\n|                  |" + "\n|                  O" + "\n|               ---|---" + "\n|                  /\\" + "\n|                 /  \\" + "\n|_______________________"};
-            Console.WriteLine(Hangman[guesses]);
+            //cleaner to not print blank line if 0 lives lost
+            if (lives > 0)
+            {
+                Console.WriteLine(Hangman[guesses]);
+            }
+        }
+
+        static void getWord()
+        {
+            //gets word to guess, either randomly from listOfWords or array or from a dictionary.txt
+            while (secretWord.Equals(""))
+            {
+                string option = "";
+                Console.WriteLine("Enter 1 for random word from array, 2 for word from dictionary");
+                option = Console.ReadLine();
+                Random random = new Random();
+                if (option.Equals("1"))
+                {
+                    secretWord = listOfWords[random.Next(listOfWords.Length)].ToLower();
+                    //currentWord as secretWord but all _
+                    currentWord = new String('_', secretWord.Length);
+                }
+                else if (option.Equals("2"))
+                {
+                    //reads all words from dictionary.txt, generates a random word
+                    string[] lines = System.IO.File.ReadAllLines("dictionary.txt");
+                    secretWord = lines[random.Next(lines.Length)].ToLower();
+                    currentWord = new String('_', secretWord.Length);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input, must enter 1 or 2");
+                }
+            }
+        }
+
+        static void getGuess()
+        {
+            input = "";
+            while (input.Equals(""))
+            {
+                Console.WriteLine("Enter a word or letter to guess:");
+                //if letter word is valid a/z and not already guessed, then is valid guess
+                input = Console.ReadLine().ToLower();
+                //valid guess has to be a-z, not used before, and one letter or word of correct length
+                if (Regex.IsMatch(input, "[a-z]", RegexOptions.IgnoreCase) && !guesses.Contains(input)
+                    && ((input.Length == 1) || (input.Length == secretWord.Length)))
+                {
+                    guesses.Add(input);
+                }
+                else if (guesses.Contains(input))
+                {
+                    Console.WriteLine("Already guessed!");
+                }
+                else 
+                {
+                    Console.WriteLine("Invalid guess, must guess a letter or word of correct length");
+                    input = "";
+                }
+            }
+        }
+        
+        static void restart()
+        {
+            Console.WriteLine("Enter y to play again, anything else to quit");
+            if (Console.ReadLine().ToLower().Equals("y"))
+            {
+                //reset all vars, run main again
+                input = "";
+                lives = 0;
+                guesses.Clear();
+                secretWord = "";
+                Main(new string[] { });
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
+        }
+
+        static void showGuesses()
+        {
+            Console.WriteLine("Guesses: ");
+            foreach (var g in guesses)
+            {
+                Console.Write(g + " ");
+            }
         }
     }
 }
